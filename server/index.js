34ae40677ba58d1881ac143846bf4508c9bdf40a
Kristina H <42981533+kristinahocevar@users.noreply.github.com>
2019-11-20 18:56:34 +0100

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const server = new ApolloServer({
 })
 
 mongoose
-  .connect(MONGODB, { useNewUrlParser: true })
+  .connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('MongoDB Connected')
     return server.listen({ port: process.env.PORT || 5000
